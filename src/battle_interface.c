46f2a91716f022a210f946061bb0730a4623d90f
SoupPotato <toomanyluigis11@gmail.com>
2023-06-20 12:48:41 +0100

    More Updates

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -1860,8 +1860,7 @@ s32 MoveBattleBar(u8 battlerId, u8 healthboxSpriteId, u8 whichBar, u8 unused)
                                           gBattleSpritesDataPtr->battleBars[battlerId].oldValue,
                                           gBattleSpritesDataPtr->battleBars[battlerId].receivedValue,
                                           &gBattleSpritesDataPtr->battleBars[battlerId].currValue,
-                                          B_HEALTHBAR_NUM_TILES,
-                                          1);
+                                          B_HEALTHBAR_NUM_PIXELS / 8, max(gBattleSpritesDataPtr->battleBars[battlerId].maxValue / 48, 1));
     }
     else // exp bar
     {
