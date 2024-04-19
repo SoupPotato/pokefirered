50996ddadd433c9aeed84dab63f243b2ad6ca00d
SoupPotato <toomanyluigis11@gmail.com>
2024-04-19 22:59:51 +0100

    Added PSS icons to move screen

--- include/pokemon.h
+++ include/pokemon.h
@@ -246,6 +246,7 @@ struct BattleMove
     u8 target;
     s8 priority;
     u8 flags;
+    u8 category;
 };
 
 #define SPINDA_SPOT_WIDTH 16
@@ -268,6 +269,13 @@ struct Evolution
     u16 method;
     u16 param;
     u16 targetSpecies;
+};
+
+enum
+{
+    MOVE_PHYSICAL,
+    MOVE_SPECIAL,
+    MOVE_STATUS
 };
 
 #define NUM_UNOWN_FORMS 28
